ebecadf07daa3b8c3d569fa7c6ebe6ad2e1b34cb
sachin <sachinsonawane.edu@gmail.com>
2017-06-03 15:55:21 +0530

    Added animation in navigation bar

--- js/navshrink.js
+++ js/navshrink.js
@@ -2,13 +2,13 @@
  * Created by SACHIN on 02-Jun-17.
  */
 $(document).ready(function() {
-
     $(window).scroll(function () {
-        if ($(window).scrollTop() > 143) {
-            $('#nav_bar').addClass('navbar-fixed-top glass animated fadeInDown');
+        console.log($(window).scrollTop())
+        if ($(window).scrollTop() > 233) {
+            $('#nav_bar').addClass('navbar-fixed-top animated fadeInDown');
         }
-        if ($(window).scrollTop() < 144) {
-            $('#nav_bar').removeClass('navbar-fixed-top glass animated fadeIn');
+        if ($(window).scrollTop() < 234) {
+            $('#nav_bar').removeClass('navbar-fixed-top animated fadeIn');
         }
     });
 });
